95b992ae687a97de9993e17df87559f33d1d4e19
galatedgi <gal.atedgi@gmail.com>
2021-06-04 13:09:44 +0300

    c

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import os
-import keras.preprocessing.image
+import matplotlib.pyplot as plt
 import numpy as np
+from keras import Sequential
+from keras.layers import BatchNormalization, GlobalAveragePooling2D, Dense, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 
 
@@ -57,7 +60,7 @@ def data_generator(data):
         width_shift_range=0.1,
         height_shift_range=0.1,
         horizontal_flip=True)
-    generator.fit(data)
+    generator.fit(np.array([x for x in data]))
     return generator
 
 
@@ -78,15 +81,76 @@ def split_data(data,test_size,valid_size):
 
     return  X_train,X_valid, X_test, y_train, y_valid,y_test,generator
 
+def get_model(name):
+    model=Sequential()
+    if name == "vgg16":
+        app=tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(IMG_SIZE,IMG_SIZE,3))
+    elif name== "densenet121":
+        app=tf.keras.applications.DenseNet121(weights='imagenet', include_top=False, input_shape=(IMG_SIZE,IMG_SIZE,3))
+    for layer in app.layers:
+        if isinstance(layer, BatchNormalization):
+            layer.trainable = True
+        else:
+            layer.trainable = False
+    model.add(app)
+    return model
+
+def add_layers_to_model(model):
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(500, activation='selu'))
+    model.add(Dropout(.4))
+    model.add(BatchNormalization())
+    model.add(Dense(256, activation='selu'))
+    model.add(Dropout(.25))
+    model.add(BatchNormalization())
+    model.add(Dense(NUM_OF_LABEL, activation='softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+
+
+def run_model(model,X_train,X_valid, X_test, y_train, y_valid,y_test,generator,batch=200):
+    es = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
+    cb_list = [es]
+    fit_model_log = model.fit(generator.flow(
+        np.array([x for x in X_train]), y_train,
+        batch_size=batch),
+        validation_data=(np.array([x for x in X_valid]), y_valid),
+        epochs=50,
+        shuffle=True,
+        callbacks=[cb_list]
+    )
+    test_score = model.evaluate(np.array([x for x in X_test]), y_test)
+
+    plt.plot(fit_model_log.history['loss'])
+    plt.plot(fit_model_log.history['val_loss'])
+    plt.title('Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    plt.plot(fit_model_log.history['accuracy'])
+    plt.plot(fit_model_log.history['val_accuracy'])
+    plt.title('Accuracy')
+    plt.ylabel('acc')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    print(model.metrics_names[0] + ":"+ str(test_score[0]))
+    print(model.metrics_names[1] + ":"+ str(test_score[1]))
+
+
+
 
 
 
 if __name__ == '__main__':
     df=get_df()
     df=get_image_data(df)
-    print(df.head())
+    # print(df.head())
     X_train,X_valid, X_test, y_train, y_valid,y_test,generator=split_data(df,0.5,0.5)
-    print(X_train.shape,y_train.shape)
-    print(X_valid.shape,y_valid.shape)
-    print(X_test.shape,y_test.shape)
+    model=get_model("vgg16")
+    add_layers_to_model(model)
+    run_model(model,X_train,X_valid, X_test, y_train, y_valid,y_test,generator)
+
 
